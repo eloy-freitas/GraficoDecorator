fbb65666784e87ef996270d13829d368404f4b61
eloy-freitas <freitaseloy.1@gmail.com>
2023-02-06 20:38:36 -0300

    feat: iniciando projeto

---
+++ src/main/java/com/ufes/graficodecorator/GraficoDecorator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ufes.graficodecorator;
+
+/**
+ *
+ * @author eloy
+ */
+public class GraficoDecorator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
